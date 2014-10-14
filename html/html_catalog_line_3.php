80aadbc877668b5d4f6054bd870fe3b3707ec5db
AutoWebOffice <AutoWebOffice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-14 17:05:09 +0000

    * FIX: Исправленна функция отображения каталога товаров на страницах сайта
    git-svn-id: https://plugins.svn.wordpress.org/autoweboffice-internet-shop/trunk@1007440 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/html_catalog_line_3.php
+++ html/html_catalog_line_3.php
@@ -1,21 +1,17 @@
 <?php
-
 $html_catalog .= '<div align="center"><table style="width: 100%;" border=1 cellpadding="5px;">';
 
 // Для подсчет номера товара
 $i = 0;
 
-// Для подсчета товаров в строке
-$g = 0;
 
-// Для накопления информации по товарам
-$mass_goods = array();
-
+$html_catalog_goods_1 = '';
+$html_catalog_goods_2 = '';
+$html_catalog_goods_3 = '';
 
 // Цикл по товарам
 foreach ($awo_goods as $key => $goods)
 {
-	$g++;
 	
 	// Если достигнут лимит товаров для одной страницы
 	if($awo_catalog_goods_per_page == $i)
@@ -23,132 +19,102 @@ foreach ($awo_goods as $key => $goods)
 		break;
 	}
 	
-	$mass_goods[$g] = $goods;
+
+	$goods_name = htmlspecialchars(trim($goods->goods));
+			
+	$url_page = $goods->url_page;
+	$image = $goods->image;
 	
-	// Если накопили данные по трем товарам
-	if($g == 3 OR count($awo_goods) < 3)
+	$goods_url = '/?page_id='.get_query_var('page_id').'&paged='.get_query_var('paged').'&id_goods='.$id_goods;
+	
+	
+	$html_catalog_goods_1 .= '<td style="padding:5px; width: 205px;">';
+	$html_catalog_goods_1 .= '<div align="center">';
+	if(trim($url_page) != '')
 	{
-		$html_catalog .= '<tr>';
-		
-		for ($n = 1; $n <= 3; $n++) 
-		{
-			// Вормируем название товара для печати на экран
-			$id_goods = (int)$mass_goods[$n]->id_goods;
-			
-			// Если не получен код товара
-			if($id_goods == 0)
-			{
-				$html_catalog .= '<td style="padding:5px; width: 205px;">';
-				$html_catalog .= '&nbsp;';
-				$html_catalog .= '</td>';
-				continue;
-			}
-			
-			$goods_name = htmlspecialchars(trim($mass_goods[$n]->goods));
-			
-			$url_page = $mass_goods[$n]->url_page;
-			$image = $mass_goods[$n]->image;
-			
-			$goods_url = '/?page_id='.get_query_var('page_id').'&paged='.get_query_var('paged').'&id_goods='.$id_goods;
-			
-			
-			$html_catalog .= '<td style="padding:5px; width: 205px;">';
-			$html_catalog .= '<div align="center">';
-			if(trim($url_page) != '')
-			{
-				$html_catalog .= '<a href="'.$url_page.'" title="Узнать подробней о '.$goods_name.'">';
-			}
+		$html_catalog_goods_1 .= '<a href="'.$url_page.'" title="Узнать подробней о '.$goods_name.'">';
+	}
 
-			// Если есть данные по изображению товара
-			if(trim($image) != '')
-			{
-				$image_url = 'https://'.$awo_storesId.'.autokassir.ru/images/goods/pics/'.$image;
-			}
-			else
-			{
-				$image_url = '/wp-content/plugins/autoweboffice-internet-shop/img/cap/goods.png';
-			}
-			
-			$html_catalog .= '<img style="vertical-align: top; height: 170px; max-width: 170px;" src="'.$image_url.'" alt="'.$goods_name.'" title="'.$goods_name.'">';
-			if(trim($url_page) != '')
-			{
-				$html_catalog .= '</a>';
-			}
-			$html_catalog .= '</div>';
-			$html_catalog .= '</td>';
-		}
-		
-		$html_catalog .= '</tr>';
-		$html_catalog .= '<tr>';
-		
-		for ($n = 1; $n <= 3; $n++) 
-		{
-			$id_goods = $mass_goods[$n]->id_goods;
-			
-			// Если не получен код товара
-			if($id_goods == 0)
-			{
-				$html_catalog .= '<td style="padding:5px; width: 205px;">';
-				$html_catalog .= '&nbsp;';
-				$html_catalog .= '</td>';
-				continue;
-			}
-			
-			$goods_name = htmlspecialchars(trim($mass_goods[$n]->goods));
-			
-			$url_page = $mass_goods[$n]->url_page;
-			
-			$goods_url = '/?page_id='.get_query_var('page_id').'&paged='.get_query_var('paged').'&id_goods='.$id_goods;
-
-			$html_catalog .= '<td style="padding:5px; width: 205px;">';
-			if(trim($url_page) != '')
-			{
-				$html_catalog .= '<a href="'.$url_page .'" title="Узнать подробней о '.$goods_name.'">';
-			}	
-			$html_catalog .= '<b>'.$goods_name.'</b>';
-			if(trim($url_page) != '')
-			{
-				$html_catalog .= '</a>';
-			}
-			$html_catalog .= '</td>';
-		}
-		
-		$html_catalog .= '</tr>';
-		$html_catalog .= '<tr>';
-		
-		// $html_catalog .= '<div>'.substr($goods->brief_description, 0, 120).'</div>';
-		
-		for ($n = 1; $n <= 3; $n++) 
-		{
-			$id_goods = $mass_goods[$n]->id_goods;
-			
-			// Если не получен код товара
-			if($id_goods == 0)
-			{
-				$html_catalog .= '<td style="padding:5px; width: 205px;">';
-				$html_catalog .= '&nbsp;';
-				$html_catalog .= '</td>';
-				continue;
-			}
-			
-			$price = $mass_goods[$n]->price;
-			
-			$html_catalog .= '<td style="padding:5px; width: 205px;">';
-			$html_catalog .= '<div style="width: 100%; height: 40px; line-height: 40px; overflow: hidden; display: block; border: 1px dashed #d2d0cb; text-align: center; font-size: 17px; border-radius: 5px; margin-bottom: 15px;}">'.$price.'</div>';
-			$html_catalog .= '<div align="center" style="padding:0px 0px 20px 0px;"><input style="width: 182px; padding: 5px;" type="submit" class="awo_add_to_cart" id="'.$id_goods.'" value="'.$awo_catalog_settings_submit_value.'" title="'.$goods_name.' - '.$awo_catalog_settings_submit_value.'"></div>';
-			$html_catalog .= '</td>';
-		}
-			
-		
-		$mass_goods = array();
-		$g = 0;
-		
-		$html_catalog .= '</tr>';
+	// Если есть данные по изображению товара
+	if(trim($image) != '')
+	{
+		$image_url = 'https://'.$awo_storesId.'.autokassir.ru/images/goods/pics/'.$image;
+	}
+	else
+	{
+		$image_url = '/wp-content/plugins/autoweboffice-internet-shop/img/cap/goods.png';
 	}
 	
-	// Сдвигаем счетчик товаров (один товар опубликовали)
-	$i++;
+	$html_catalog_goods_1 .= '<img style="vertical-align: top; height: 170px; max-width: 170px;" src="'.$image_url.'" alt="'.$goods_name.'" title="'.$goods_name.'">';
+	if(trim($url_page) != '')
+	{
+		$html_catalog_goods_1 .= '</a>';
+	}
+	$html_catalog_goods_1 .= '</div>';
+	$html_catalog_goods_1 .= '</td>';
 
+
+			
+	$goods_name = htmlspecialchars(trim($goods->goods));
+	
+	$url_page = $goods->url_page;
+	
+	$goods_url = '/?page_id='.get_query_var('page_id').'&paged='.get_query_var('paged').'&id_goods='.$id_goods;
+
+	$html_catalog_goods_2 .= '<td style="padding:5px; width: 205px;">';
+	if(trim($url_page) != '')
+	{
+		$html_catalog_goods_2 .= '<a href="'.$url_page .'" title="Узнать подробней о '.$goods_name.'">';
+	}	
+	$html_catalog_goods_2 .= '<b>'.$goods_name.'</b>';
+	if(trim($url_page) != '')
+	{
+		$html_catalog_goods_2 .= '</a>';
+	}
+	$html_catalog_goods_2 .= '</td>';
+		
+
+			
+	$price = $goods->price;
+	
+	$html_catalog_goods_3 .= '<td style="padding:5px; width: 205px;">';
+	$html_catalog_goods_3 .= '<div style="width: 100%; height: 40px; line-height: 40px; overflow: hidden; display: block; border: 1px dashed #d2d0cb; text-align: center; font-size: 17px; border-radius: 5px; margin-bottom: 15px;}">'.$price.'</div>';
+	$html_catalog_goods_3 .= '<div align="center" style="padding:0px 0px 20px 0px;"><input style="width: 182px; padding: 5px;" type="submit" class="awo_add_to_cart" id="'.$id_goods.'" value="'.$awo_catalog_settings_submit_value.'" title="'.$goods_name.' - '.$awo_catalog_settings_submit_value.'"></div>';
+	$html_catalog_goods_3 .= '</td>';
+	
+	$i++;
+	
+	// Если есть три товара в строке
+	if($i % 3 == 0)
+	{
+		$html_catalog .= '<tr>';
+		$html_catalog .= $html_catalog_goods_1;
+		$html_catalog .= '</tr>';
+		$html_catalog .= '<tr>';
+		$html_catalog .= $html_catalog_goods_2;
+		$html_catalog .= '</tr>';
+		$html_catalog .= '<tr>';
+		$html_catalog .= $html_catalog_goods_3;
+		$html_catalog .= '</tr>';
+		
+		$html_catalog_goods_1 = '';
+		$html_catalog_goods_2 = '';
+		$html_catalog_goods_3 = '';
+	}
+
+}
+
+if(trim($html_catalog_goods_1) != '')
+{
+	$html_catalog .= '<tr>';
+	$html_catalog .= $html_catalog_goods_1;
+	$html_catalog .= '</tr>';
+	$html_catalog .= '<tr>';
+	$html_catalog .= $html_catalog_goods_2;
+	$html_catalog .= '</tr>';
+	$html_catalog .= '<tr>';
+	$html_catalog .= $html_catalog_goods_3;
+	$html_catalog .= '</tr>';
 }
 
 $html_catalog .= '</table></div>';
